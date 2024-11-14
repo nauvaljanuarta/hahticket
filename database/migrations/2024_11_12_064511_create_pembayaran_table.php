17f046b61c64b276efc940182d7b630cb4979468
ndajatoh01 <nauvalaryanto2005@gmail.com>
2024-11-14 10:22:19 +0700

    tambah database

--- database/migrations/2024_11_12_064511_create_pembayaran_table.php
+++ database/migrations/2024_11_12_064511_create_pembayaran_table.php
@@ -13,13 +13,13 @@ return new class extends Migration
     {
         Schema::create('pembayaran', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('pemesanan_id');
             $table->unsignedBigInteger('event_ticket_id');
             $table->unsignedBigInteger('jenis_pembayaran');
             $table->integer('jumlah');
             $table->string('status', 20);
             $table->timestamps();
-            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('pemesanan_id')->references('id')->on('pemesanan');
             $table->foreign('event_ticket_id')->references('id')->on('event_tickets');
             $table->foreign('jenis_pembayaran')->references('id')->on('jenis_pembayaran');
         });
